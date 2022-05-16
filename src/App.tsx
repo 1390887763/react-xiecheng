09d448547f3079a2db3d990e94d25697019621e8
Wangshaoyu <wangshaoyu@WangshaoyudeMacBook-Pro.local>
2022-05-16 17:04:23 +0800

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,16 @@
-// asd
 import React from 'react';
 import styles from './App.module.css'
-import {Header, Footer, SideMenu, Carousel, ProductCollection, Cooperation} from "./components"
-import { Row, Col, Typography } from "antd"
-import { productList1, productList2, productList3 } from './mockups'
-import sideimage1 from './assets/images/sider_2019_02-04.png'
-import sideimage3 from './assets/images/sider_2019_02-04-2.png'
-import sideimage2 from './assets/images/sider_2019_12-09.png'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HomePage } from "./pages";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header />
-      {/* 页面内容 */}
-      <div className={styles['page-content']}>
-        <Row style={{marginTop: 20}}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-          <ProductCollection
-            title={<Typography.Title level={3} type="warning">爆款推荐</Typography.Title>}
-            sideImage={sideimage1}
-            products={productList1}
-          >
-          </ProductCollection>
-          <ProductCollection
-            title={<Typography.Title level={3} type="danger">新品上市</Typography.Title>}
-            sideImage={sideimage2}
-            products={productList2}
-          >
-          </ProductCollection>
-          <ProductCollection
-            title={<Typography.Title level={3} type="success">国内游推荐</Typography.Title>}
-            sideImage={sideimage3}
-            products={productList3}
-          >
-          </ProductCollection>
-        </Row>
-        <Cooperation />
-      </div>
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
